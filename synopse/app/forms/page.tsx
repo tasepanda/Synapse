8e966ce4ce1f8610cf9b8fe3e8a6d445c7df67d5
Kei <k.tare.bee21@gmail.com>
2025-02-11 19:44:31 +0900

    modify some config

--- synopse/app/forms/page.tsx
+++ synopse/app/forms/page.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { ChevronDownIcon } from '@heroicons/react/16/solid'
-import { Field, Label, Switch } from '@headlessui/react'
+import { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import { Field, Label, Switch } from "@headlessui/react";
 
 export default function Example() {
-  const [agreed, setAgreed] = useState(false)
+  const [agreed, setAgreed] = useState(false);
 
   return (
     // <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
     <div className="isolate bg-[url('https://sd-webworks.com/wp-content/uploads/2021/06/win1-1024x640.jpg')] bg-cover bg-no-repeat px-6 py-24 sm:py-32 lg:px-8">
-  {/* コンテンツ */}
+      {/* コンテンツ */}
       <div
         aria-hidden="true"
         className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
@@ -18,19 +18,30 @@ export default function Example() {
         <div
           style={{
             clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
           className="relative left-1/2 -z-10 aspect-1155/678 w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
         />
       </div>
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact sales</h2>
-        <p className="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+        <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
+          Contact sales
+        </h2>
+        <p className="mt-2 text-lg/8 text-gray-600">
+          Aute magna irure deserunt veniam aliqua magna enim voluptate.
+        </p>
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form
+        action="#"
+        method="POST"
+        className="mx-auto mt-16 max-w-xl sm:mt-20"
+      >
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
+            <label
+              htmlFor="first-name"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
               First name
             </label>
             <div className="mt-2.5">
@@ -44,7 +55,10 @@ export default function Example() {
             </div>
           </div>
           <div>
-            <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
+            <label
+              htmlFor="last-name"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
               Last name
             </label>
             <div className="mt-2.5">
@@ -58,7 +72,10 @@ export default function Example() {
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="company" className="block text-sm/6 font-semibold text-gray-900">
+            <label
+              htmlFor="company"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
               Company
             </label>
             <div className="mt-2.5">
@@ -72,7 +89,10 @@ export default function Example() {
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
               Email
             </label>
             <div className="mt-2.5">
@@ -86,7 +106,10 @@ export default function Example() {
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
+            <label
+              htmlFor="phone-number"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
               Phone number
             </label>
             <div className="mt-2.5">
@@ -119,7 +142,10 @@ export default function Example() {
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
+            <label
+              htmlFor="message"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
               Message
             </label>
             <div className="mt-2.5">
@@ -128,7 +154,7 @@ export default function Example() {
                 name="message"
                 rows={4}
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                defaultValue={''}
+                defaultValue={""}
               />
             </div>
           </div>
@@ -147,7 +173,7 @@ export default function Example() {
               </Switch>
             </div>
             <Label className="text-sm/6 text-gray-600">
-              By selecting this, you agree to our{' '}
+              By selecting this, you agree to our{" "}
               <a href="#" className="font-semibold text-indigo-600">
                 privacy&nbsp;policy
               </a>
@@ -165,5 +191,5 @@ export default function Example() {
         </div>
       </form>
     </div>
-  )
+  );
 }
