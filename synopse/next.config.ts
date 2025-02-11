8d5f0bdce194fa40b60b2c62b891bd7acd286d40
Kei <k.tare.bee21@gmail.com>
2025-02-11 19:03:45 +0900

    configの調整

--- synopse/next.config.ts
+++ synopse/next.config.ts
@@ -8,10 +8,9 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com', 'www.jsports.co.jp'], // 許可するドメインを追加
   },
+  rules: {
+    'no-unescaped-entities': 'off', // ルールを無効化
+  },
 };
-
-rules: {
-  'no-unescaped-entities': 'off', // ルールを無効化
-},
 
 export default nextConfig;
