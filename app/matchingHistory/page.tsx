2cae7b846c2377fc08e413d7ff050260ac945f08
{Keisuke <{k.tare.bee21@gmail.com}>
2025-02-16 15:44:36 +0900

    modify matching history

--- app/matchingHistory/page.tsx
+++ app/matchingHistory/page.tsx
@@ -3,10 +3,13 @@ import History from "../components/matchingHistory";
 
 export default function result() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h2 className="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl text-center">
-        これまでにマッチした人の履歴をここから閲覧できます
+    <div className="grid grid-rows-[20px_5px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h2 className="text-3xl font-semibold tracking-tight text-pretty p-8 pb-20 text-gray-900 sm:text-4xl text-center">
+        Matching History
       </h2>
+      <h2>マッチング日付:2025/1/20</h2>
+      <History />
+      <h2>マッチング日付:2025/1/14</h2>
       <History />
     </div>
   );
