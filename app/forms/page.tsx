73885bab10af4b4b545ec91f2015ea3da5714eae
{Keisuke <{k.tare.bee21@gmail.com}>
2025-02-18 23:06:42 +0900

    modify from hattori

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -6,10 +6,27 @@ import { Field, Label, Switch } from "@headlessui/react";
 
 export default function Example() {
   const [agreed, setAgreed] = useState(false);
+  const [position, setAge] = useState("");
+  const [roles, setRoles] = useState([]); // 役割を管理するstate
+
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+
+  const handleRoleChange = (e) => {
+    const role = e.target.value;
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+      setRoles([...roles, role]); // 選択された役割を追加
+    } else {
+      setRoles(roles.filter((r) => r !== role)); // 選択解除された役割を削除
+    }
+  };
 
   return (
     // <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-    <div className="isolate bg-[url('https://sd-webworks.com/wp-content/uploads/2021/06/win1-1024x640.jpg')] bg-cover bg-no-repeat px-6 py-24 sm:py-32 lg:px-8">
+    <div className="isolate bg-[url('https://sozaino.site/wp-content/uploads/2023/10/okumono-art10.png')] bg-cover bg-no-repeat px-6 py-24 sm:py-32 lg:px-8">
       {/* コンテンツ */}
       <div
         aria-hidden="true"
@@ -25,11 +42,9 @@ export default function Example() {
       </div>
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
-          Contact sales
+          登録画面
         </h2>
-        <p className="mt-2 text-lg/8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
-        </p>
+        <p className="mt-2 text-lg/8 text-gray-600">Synapseへようこそ!</p>
       </div>
       <form
         action="#"
@@ -39,15 +54,15 @@ export default function Example() {
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label
-              htmlFor="first-name"
+              htmlFor="last-name"
               className="block text-sm/6 font-semibold text-gray-900"
             >
-              First name
+              苗字
             </label>
             <div className="mt-2.5">
               <input
-                id="first-name"
-                name="first-name"
+                id="last-name"
+                name="last-name"
                 type="text"
                 autoComplete="given-name"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
@@ -56,36 +71,130 @@ export default function Example() {
           </div>
           <div>
             <label
-              htmlFor="last-name"
+              htmlFor="first-name"
               className="block text-sm/6 font-semibold text-gray-900"
             >
-              Last name
+              名前
             </label>
             <div className="mt-2.5">
               <input
-                id="last-name"
-                name="last-name"
+                id="first-name"
+                name="first-name"
                 type="text"
                 autoComplete="family-name"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               />
             </div>
           </div>
+
           <div className="sm:col-span-2">
             <label
-              htmlFor="company"
+              htmlFor="Department"
               className="block text-sm/6 font-semibold text-gray-900"
             >
-              Company
+              部署・室
             </label>
             <div className="mt-2.5">
-              <input
-                id="company"
-                name="company"
-                type="text"
-                autoComplete="organization"
+              <div className="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                <div className="grid shrink-0 grid-cols-1 focus-within:relative">
+                  <select
+                    id="department"
+                    name="department"
+                    autoComplete="department"
+                    aria-label="department"
+                    className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  >
+                    <option>開発・製造IT推進部</option>
+                    <option>ビジネスIT推進部</option>
+                    <option>情報トラスト・セキュリティ</option>
+                  </select>
+                  <ChevronDownIcon
+                    aria-hidden="true"
+                    className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                  />
+                </div>
+                <input
+                  id="division"
+                  name="division"
+                  type="text"
+                  placeholder="⚪︎⚪︎室"
+                  className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            {" "}
+            {/* 年齢層の選択式 */}
+            <label
+              htmlFor="age"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
+              職位
+            </label>
+            <div className="mt-2.5">
+              <select
+                id="position"
+                name="position"
+                value={position}
+                onChange={handleAgeChange}
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-              />
+              >
+                <option value="">選択してください</option>
+                <option value="指導職以下">指導職以下</option>
+                <option value="主任">主任</option>
+                <option value="主幹">主幹</option>
+                <option value="主査">主査</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            {" "}
+            {/* 役割の複数選択 */}
+            <label
+              className="block text-sm/6 font-semibold text-gray-900"
+              style={{ color: "black" }}
+            >
+              役割 (複数選択可)
+            </label>
+            <div
+              className="mt-2.5 flex flex-col gap-2"
+              style={{ color: "black" }}
+            >
+              {" "}
+              {/* flex-colで縦に並べる */}
+              <label>
+                <input
+                  type="checkbox"
+                  value="PM"
+                  checked={roles.includes("PM")}
+                  onChange={handleRoleChange}
+                  className="mr-2" // checkboxとlabelの間にスペース
+                />
+                PM
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="運用維持"
+                  checked={roles.includes("運用維持")}
+                  onChange={handleRoleChange}
+                  className="mr-2"
+                />
+                運用維持
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="企画"
+                  checked={roles.includes("企画")}
+                  onChange={handleRoleChange}
+                  className="mr-2"
+                />
+                企画
+              </label>
             </div>
           </div>
           <div className="sm:col-span-2">
@@ -93,7 +202,7 @@ export default function Example() {
               htmlFor="email"
               className="block text-sm/6 font-semibold text-gray-900"
             >
-              Email
+              領域
             </label>
             <div className="mt-2.5">
               <input
@@ -105,53 +214,18 @@ export default function Example() {
               />
             </div>
           </div>
+
           <div className="sm:col-span-2">
             <label
-              htmlFor="phone-number"
+              htmlFor="problem"
               className="block text-sm/6 font-semibold text-gray-900"
             >
-              Phone number
-            </label>
-            <div className="mt-2.5">
-              <div className="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-                <div className="grid shrink-0 grid-cols-1 focus-within:relative">
-                  <select
-                    id="country"
-                    name="country"
-                    autoComplete="country"
-                    aria-label="Country"
-                    className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  >
-                    <option>US</option>
-                    <option>CA</option>
-                    <option>EU</option>
-                  </select>
-                  <ChevronDownIcon
-                    aria-hidden="true"
-                    className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                  />
-                </div>
-                <input
-                  id="phone-number"
-                  name="phone-number"
-                  type="text"
-                  placeholder="123-456-7890"
-                  className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm/6 font-semibold text-gray-900"
-            >
-              Message
+              課題
             </label>
             <div className="mt-2.5">
               <textarea
-                id="message"
-                name="message"
+                id="problem"
+                name="problem"
                 rows={4}
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                 defaultValue={""}
@@ -186,7 +260,7 @@ export default function Example() {
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Lets talk
+            登録
           </button>
         </div>
       </form>
